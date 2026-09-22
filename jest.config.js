b8e14a3d0c9d2207fb076a646e4510d5d6efddf6
Przemysław Turek <przemyslaw@mol.com.pl>
2026-09-22 08:24:37 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,7 @@ module.exports = {
     ...config.setupFilesAfterEnv,
     path.join(__dirname, './test/jest/jest.setup.js'),
   ],
+   transformIgnorePatterns: [
+    '/node_modules/(?!(@folio|@json2csv|decode-uri-component|filter-obj|find-up|get-stdin|global-dirs|import-lazy|inquirer|is-path-inside|jspdf|keyboardjs|ky|query-string|resolve-from|resolve-pkg|split-on-first|uuid)/)',
+  ],
 };
