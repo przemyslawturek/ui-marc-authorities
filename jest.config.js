6c827bffd66f392a1a3cb4087b6d580c4d0b75e3
Przemysław Turek <przemyslaw@mol.com.pl>
2026-09-23 07:58:52 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const config = require('@folio/jest-config-stripes');
 
+const additionalModules = [
+  'keyboardjs',
+].join('|');
+const combinedModules = config.transformIgnorePatterns[0].replace(')', `|${additionalModules})`);
+
 module.exports = {
   ...config,
   setupFiles: [
@@ -11,7 +16,5 @@ module.exports = {
     ...config.setupFilesAfterEnv,
     path.join(__dirname, './test/jest/jest.setup.js'),
   ],
-   transformIgnorePatterns: [
-    '/node_modules/(?!(@folio|@json2csv|decode-uri-component|filter-obj|find-up|get-stdin|global-dirs|import-lazy|inquirer|is-path-inside|jspdf|keyboardjs|ky|query-string|resolve-from|resolve-pkg|split-on-first|uuid)/)',
-  ],
+  transformIgnorePatterns: [combinedModules],
 };
